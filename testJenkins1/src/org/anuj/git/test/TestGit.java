e705896c1927a0a24434f7bb6767f49c76a71f81
Anuj <pr.anuj108@gmail.com>
2021-03-28 14:40:33 +0530

    First Git Test Project

---
+++ testJenkins1/src/org/anuj/git/test/TestGit.java
@@ -0,0 +1,15 @@
+package org.anuj.git.test;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		TestGit testGit = new TestGit();
+		testGit.printMessage();
+
+	}
+
+	private  void  printMessage() {
+		System.out.print("Hello This is from Git TestGit.java");
+	}
+
+}
